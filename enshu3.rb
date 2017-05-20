5184cbe9e999d681264d6270ef4e391033238ef0
tetsuya_ogawa <tetsuya.ogawa87@gmail.com>
2017-05-20 22:01:30 +0900

    演習3とnokogiriを軽く動かした

---
+++ enshu3.rb
@@ -0,0 +1,67 @@
+# この問題はmoduleのメソッドが呼び出される際のselfを意識する
+
+require 'date'
+module Mail
+
+  def write(body)
+    @body = body
+  end
+
+  def send
+    @send_time = DateTime.now
+    puts create_sended_time_info + 'Sending a Postcard to ' + @recipient + '.'
+  end
+
+  def to_s
+    ["#{create_sended_time_info}\n", "Dear #{@recipient},\n", "#{@body}\n", "Sincerely \n#{@sender}"].join("\n")
+  end
+
+  private
+
+  def create_sended_time_info
+    @send_time.strftime('%a %b %d %X %Z %Y: ')
+  end
+end
+
+class Postcard
+  include Mail
+
+  def initialize(sender, recipient)
+    @sender, @recipient = sender, recipient
+  end
+end
+
+card = Postcard.new("Me", "Friend")
+card.write("Did you forget my birthsday?")
+card.send
+puts "-" * 60
+sleep(2)
+puts card
+
+# Tue Mar 22 11:59:16 +0900 2011: Sending a Postcard to Friend.
+# ------------------------------------------------------------
+# Postcard of Tue Mar 22 11:59:16 +0900 2011:
+#
+# Dear Friend,
+#
+# Did you forget my birthsday?
+#
+# Sincerely,
+# Me
+
+# 模範解答 PostCardがクラスじゃなくて文字列だと思っていた(笑)
+# module Mail
+#   attr_reader :sender, :recipient, :text, :date
+#
+#   def send
+#     puts "#{@date = Time.now}: Sending a #{self.class} to #{self.recipient}."
+#   end
+#
+#   def write(text)
+#     @text = text
+#   end
+#
+#   def to_s
+#     "#{self.class} of #{@date}:\n\nDear #{@recipient},\n\n#{@text}\n\nSincerely,\n#{@sender}"
+#   end
+# end
