c4d5b9615347a757f7a2f3d269ec4ef7c909e065
PT1972 <tetsuya.ogawa87@gmail.com>
2017-10-22 12:12:23 +0900

    rm

--- sample_each.rb
+++
@@ -1,8 +0,0 @@
-require './modules/SampleEach' # moduleのファイルをloadして
-include SampleEach             # ここでmoduleを読み込む
-
-['せ' ,'い', 'こ', 'う'].sample_each do |t|
-  puts t + 'うえ〜い'
-end
-
-['だめ', 'だめ', 'だめ', 'だめ'].sample_each
